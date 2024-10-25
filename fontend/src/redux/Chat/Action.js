d151707c7795f0164444ff96caaf2fb32c33d0a6
phamvanquochuy <102366023+phamvanquochuy@users.noreply.github.com>
2024-10-25 11:03:42 +0700

    create Chat reducer

---
+++ fontend/src/redux/Chat/Action.js
@@ -0,0 +1,62 @@
+import api from "@Api/api";
+import * as actionTypes from "./ActionTypes";
+
+export const sendMessage = (messageData) => {
+  return async (dispatch) => {
+    dispatch({ type: actionTypes.SEND_MESSAGE_REQUEST });
+    try {
+      const response = await api.post("/api/message/send", messageData);
+      dispatch({
+        type: actionTypes.SEND_MESSAGE_SUCCESS,
+        message: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: actionTypes.SEND_MESSAGE_FAILURE,
+        error: error.message,
+      });
+    }
+  };
+};
+
+export const fetchChatByProject = (projectId) => {
+  return async (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_CHAT_BY_PROJECT_REQUEST });
+    try {
+      const response = await api.get(`/api/project/${projectId}/chat`);
+      console.log("fetch chat ", response.data);
+      dispatch({
+        type: actionTypes.FETCH_CHAT_BY_PROJECT_SUCCESS,
+        chat: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: actionTypes.FETCH_CHAT_BY_PROJECT_FAILURE,
+        error: error.message,
+      });
+    }
+  };
+};
+
+export const fetchChatMessage = (chatId) => {
+  return async (dispatch) => {
+    dispatch({ type: actionTypes.FETCH_CHAT_MESSAGE_REQUEST });
+    try {
+      const response = await api.get(`/api/message/chat/${chatId}`);
+      console.log("fetch message ", response.data);
+      dispatch({
+        type: actionTypes.FETCH_CHAT_MESSAGE_SUCCESS,
+        chatId,
+        chat: response.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({
+        type: actionTypes.FETCH_CHAT_MESSAGE_FAILURE,
+        error: error.message,
+      });
+    }
+  };
+};
---
+++ fontend/src/redux/Chat/Reducer.js
@@ -0,0 +1,45 @@
+import * as actionTypes from "./ActionTypes";
+
+const initialState = {
+  messages: [],
+  loading: false,
+  error: null,
+  chat: null,
+};
+
+const chatReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.FETCH_MESSAGE_REQUEST:
+    case actionTypes.SEND_MESSAGE_REQUEST:
+    case actionTypes.FETCH_CHAT_MESSAGE_REQUEST:
+      return { ...state, loading: true, error: null };
+
+    case actionTypes.FETCH_MESSAGES_SUCCESS:
+    case actionTypes.FETCH_CHAT_MESSAGE_SUCCESS:
+      return { ...state, loading: false, messages: action.message };
+
+    case actionTypes.SEND_MESSAGE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        messages: [...state.messages, action.message],
+      };
+    case actionTypes.FETCH_CHAT_BY_PROJECT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        chat: action.chat,
+      };
+    case actionTypes.FETCH_MESSAGES_FAILURE:
+    case actionTypes.SEND_MESSAGE_FAILURE:
+    case actionTypes.FETCH_CHAT_MESSAGE_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+
+    default:
+      state;
+  }
+};
